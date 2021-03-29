009516a3afca07534422d126405057bbf286c46d
Peter Van Ausdeln <pvanaus@ucdavis.edu>
2021-03-29 10:22:02 -0700

    basic V0 for value sets

--- web/frontend/src/app/component/value-sets/value-sets.component.ts
+++ web/frontend/src/app/component/value-sets/value-sets.component.ts
@@ -10,10 +10,6 @@ export class ValueSetsComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-    setTimeout(() => {
-      window.open('https://ncit.nci.nih.gov/ncitbrowser/ajax?action=create_src_vs_tree&nav_type=valuesets&mode=0')
-    }, 10000);
-  }
+  ngOnInit() { }
 
 }
