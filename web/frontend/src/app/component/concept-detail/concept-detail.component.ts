636f06a9f8b8cda41832f58bd5c29c61e660a573
Peter Van Ausdeln <pvanausdeln@westcoastinformatics.com>
2021-05-18 15:49:35 -0700

    Rdf107 (#61)
    * remove PDQ links
    
    * align CAS_Registry link with ncit browser
    
    * fix another link
    
    * fix NSC Number link
    
    * part 3

--- web/frontend/src/app/component/concept-detail/concept-detail.component.ts
+++ web/frontend/src/app/component/concept-detail/concept-detail.component.ts
@@ -15,19 +15,19 @@ export class ConceptDetailComponent implements OnInit {
   // TODO: this needs to be pulled from the endpoint (e.g. application metadata)
   externalLinks = new Map(
     [
-      ['CAS_Registry', 'http://chem.nlm.nih.gov/chemidplus/direct.jsp?regno='],
+      ['CAS_Registry', 'https://chem.nlm.nih.gov/chemidplus/rn/'],
       ['CHEBI_ID', 'http://www.ebi.ac.uk/chebi/advancedSearchFT.do?searchString='],
       ['EntrezGene_ID', 'http://www.ncbi.nlm.nih.gov/sites/entrez?Db=gene&amp;cmd=ShowDetailView&amp;TermToSearch='],
       ['GenBank_Accession_Number', 'http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?db=gene&amp;cmd=search&amp;term=+'],
       ['HGNC_ID', 'https://www.genenames.org/data/gene-symbol-report/#!/hgnc_id/'],
       ['miRBase_ID', 'http://www.mirbase.org/cgi-bin/mirna_entry.pl?acc='],
+      ['NCI_Drug_Dictionary_ID', 'https://www.cancer.gov/publications/dictionaries/cancer-drug/def/'],
       ['NCI_META_CUI', 'https://ncim.nci.nih.gov/ncimbrowser/ConceptReport.jsp?dictionary=NCI%20MetaThesaurus&code='],
-      ['NSC_Code', 'https://dtp.cancer.gov/dtpstandard/servlet/dwindex?searchtype=NSC&amp;searchlist='],
+      ['NSC Number', ' https://dtp.cancer.gov/dtpstandard/servlet/dwindex?searchtype=NSC&outputformat=html&searchlist='],
       ['OMIM_Number', 'https://omim.org/entry/'],
-      ['PDQ_Open_Trial_Search_ID', 'http://www.cancer.gov/Search/ClinicalTrialsLink.aspx?idtype=1&amp;id='],
       ['PubMedID_Primary_Reference', 'http://www.ncbi.nlm.nih.gov:80/entrez/query.fcgi?cmd=Retrieve&amp;db=PubMed&amp;list_uids='],
       ['Swiss_Prot', 'https://www.uniprot.org/uniprot/'],
-      ['UMLS_CUI', 'https://ncim.nci.nih.gov/ncimbrowser/ConceptReport.jsp?dictionary=NCI%20MetaThesaurus&code=']
+      ['UMLS_CUI', 'https://ncim.nci.nih.gov/ncimbrowser/ConceptReport.jsp?dictionary=NCI%20MetaThesaurus&type=synonym&code=']
     ]
   )
 
