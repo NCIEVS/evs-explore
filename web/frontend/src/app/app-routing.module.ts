9f288ea12f9d31f68c6eded3f6764d2e621b8eba
Peter Van Ausdeln <pvanausdeln@westcoastinformatics.com>
2021-05-25 18:05:28 -0700

    Rdf117 (#62)
    * fix columns not having default settings (#53)
    
    * Rdf107 (#61)
    
    * remove PDQ links
    
    * align CAS_Registry link with ncit browser
    
    * fix another link
    
    * fix NSC Number link
    
    * part 3
    
    * quick copy-paste implementation
    
    * modularizing the tables
    
    * small css stuff + link in doc menu
    
    Co-authored-by: Brian Carlsen <bcarlsen@westcoastinformatics.com>

--- web/frontend/src/app/app-routing.module.ts
+++ web/frontend/src/app/app-routing.module.ts
@@ -17,6 +17,7 @@ import { RolesComponent } from './component/documentation/roles/roles.component'
 import { ContactUsComponent } from './component/contact-us/contact-us.component';
 import { DefinitionTypesComponent } from './component/documentation/definition-types/definition-types.component';
 import { SynonymTypesComponent } from './component/documentation/synonym-types/synonym-types.component';
+import { AlldocsComponent } from './component/documentation/alldocs/alldocs.component';
 // import { WelcomeComponent } from './component/welcome/welcome.component';
 
 // Routes defined
@@ -33,6 +34,7 @@ const routes: Routes = [
   { path: 'termtypes', component: TermTypesComponent },
   { path: 'definitiontypes', component: DefinitionTypesComponent },
   { path: 'synonymtypes', component: SynonymTypesComponent },
+  { path: 'alldocs', component: AlldocsComponent },
   { path: 'sources', component: SourcesComponent },
   { path: 'roles', component: RolesComponent },
   { path: 'welcome', component: GeneralSearchComponent },
--- web/frontend/src/app/app.module.ts
+++ web/frontend/src/app/app.module.ts
@@ -83,6 +83,7 @@ import { AutofocusDirective } from './directive/autofocus/autofocus.directive';
 // Cookies
 import { CookieService } from 'ngx-cookie-service';
 import { ContactUsComponent } from './component/contact-us/contact-us.component';
+import { AlldocsComponent } from './component/documentation/alldocs/alldocs.component';
 
 // Angular configuration for this application
 @NgModule({
@@ -113,7 +114,8 @@ import { ContactUsComponent } from './component/contact-us/contact-us.component'
     SourcesComponent,
     WelcomeComponent,
     AutofocusDirective,
-    ContactUsComponent
+    ContactUsComponent,
+    AlldocsComponent
   ],
   imports: [
     NgbModule,
