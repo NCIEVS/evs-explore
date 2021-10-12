2176ee6417457ac57f30024b6f87dc945775e88e
Peter Van Ausdeln <pvanausdeln@westcoastinformatics.com>
2021-10-12 13:35:48 -0700

    fix 163, some changes will revert with RDF-173 (#98)

--- web/frontend/src/app/component/concept-relationship/concept-relationship.component.ts
+++ web/frontend/src/app/component/concept-relationship/concept-relationship.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { SortEvent } from 'primeng/api';
+import { ConfigurationService } from 'src/app/service/configuration.service';
 import { ConceptDisplayComponent } from '../concept-display/concept-display.component';
 import { Concept } from './../../model/concept';
 
@@ -19,13 +20,26 @@ export class ConceptRelationshipComponent implements OnInit {
   terminology = this.cookieService.get('term');
 
   constructor(private cookieService: CookieService,
-    private conceptDisplayService: ConceptDisplayComponent) { }
+    private conceptDisplayService: ConceptDisplayComponent,
+    private configService: ConfigurationService) { }
 
   ngOnInit() {
   }
 
-  getSelectedSource() {
+  getSelectedSources(): Set<String> {
     return this.conceptDisplayService.selectedSources;
+  }
+
+  getTerminology(): String {
+    return this.configService.getTerminology().terminology;
+  }
+
+  sourceParentCheck(parent): Boolean {
+    return ((parent.source == this.getSelectedSources() || this.getSelectedSources().has('All')));
+  }
+
+  sourceAssociationCheck(association): Boolean {
+    return (association.source == this.getSelectedSources() || this.getSelectedSources().has('All'));
   }
 
   customSort(event: SortEvent) {
