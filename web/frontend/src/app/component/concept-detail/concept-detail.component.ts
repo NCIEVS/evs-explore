a7080486af179a70e58e386f259176bf0f0c06dd
Peter Van Ausdeln <pvanaus@ucdavis.edu>
2021-11-17 16:15:53 -0800

    refactor and try again

--- web/frontend/src/app/component/concept-detail/concept-detail.component.ts
+++ web/frontend/src/app/component/concept-detail/concept-detail.component.ts
@@ -24,12 +24,10 @@ export class ConceptDetailComponent implements OnInit {
       ['HGNC_ID', 'https://www.genenames.org/data/gene-symbol-report/#!/hgnc_id/'],
       ['miRBase_ID', 'http://www.mirbase.org/cgi-bin/mirna_entry.pl?acc='],
       ['NCI_Drug_Dictionary_ID', 'https://www.cancer.gov/publications/dictionaries/cancer-drug/def/'],
-      ['NCI_META_CUI', window.location.origin + '/concept/ncim/'],
       ['NSC Number', ' https://dtp.cancer.gov/dtpstandard/servlet/dwindex?searchtype=NSC&outputformat=html&searchlist='],
       ['OMIM_Number', 'https://omim.org/entry/'],
       ['PubMedID_Primary_Reference', 'http://www.ncbi.nlm.nih.gov:80/entrez/query.fcgi?cmd=Retrieve&amp;db=PubMed&amp;list_uids='],
-      ['Swiss_Prot', 'https://www.uniprot.org/uniprot/'],
-      ['UMLS_CUI', window.location.origin + '/concept/ncim/']
+      ['Swiss_Prot', 'https://www.uniprot.org/uniprot/']
     ]
   )
 
@@ -69,12 +67,7 @@ export class ConceptDetailComponent implements OnInit {
       let values = [];
       let link = this.externalLinks.get(property.type);
       let value = '';
-      if (property.type in ["NCI_META_CUI", "UMLS_CUI"]) {
-        value = '<a [routerLink]="/concept/ncim/' + property.value + '" target="_blank">' + property.value + '</a>';
-      }
-      else {
-        value = '<a href="' + link + property.value + '" target="_blank">' + property.value + '</a>';
-      }
+      value = '<a href="' + link + property.value + '" target="_blank">' + property.value + '</a>';
       return this.sanitizer.bypassSecurityTrustHtml(value);
     } else {
       return property.value;
