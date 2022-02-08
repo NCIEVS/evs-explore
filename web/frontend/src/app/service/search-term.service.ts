d7f1e007cd2a14be868679406249d3af006d9f4b
peter-va <pvanaus@ucdavis.edu>
2022-02-08 13:09:41 -0800

    221

--- web/frontend/src/app/service/search-term.service.ts
+++ web/frontend/src/app/service/search-term.service.ts
@@ -32,7 +32,7 @@ export class SearchTermService {
     // Setup search parameters (default terminology and include, for now)
     param.terminology = this.configService.getTerminologyName();
     param.include = searchCriteria.include;
-    param.term = searchCriteria.term;
+    param.term = encodeURI(searchCriteria.term);
     param.type = searchCriteria.type;
 
     // Paging parameters
@@ -88,7 +88,7 @@ export class SearchTermService {
     // }
 
     // Perform the HTTP call
-    return this.http.get(encodeURI(url),
+    return this.http.get((url),
       {
         responseType: 'json',
         params: param
