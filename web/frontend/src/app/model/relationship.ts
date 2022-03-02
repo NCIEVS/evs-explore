14df75de56d0821e1d8a1cbbeabc6fdc636c9191
Brian Carlsen <bcarlsen@westcoastinformatics.com>
2022-03-02 01:48:53 +0000

    Some fixes to handle type/rela and qualifiers properly

--- web/frontend/src/app/model/relationship.ts
+++ web/frontend/src/app/model/relationship.ts
@@ -3,12 +3,12 @@ import { ConfigurationService } from '../service/configuration.service';
 // Relationship Details - UI Component
 export class Relationship {
   type: string;
-  rela: string;
+  // rela: string;
   relatedCode: string;
   relatedName: string;
   source: string
   highlight: string;
-  smqQualifiers: any;
+  qualifiers: any;
 
   // Construct a concept reference from json input
   constructor(input: any,
@@ -16,10 +16,11 @@ export class Relationship {
   ) {
     Object.assign(this, input);
 
-    var relaqualifier = null;
+    var relaQualifier = null;
     if (input.qualifiers) {
-      relaqualifier = input.qualifiers.find(function (item) { return item.type == 'RELA'; });
-      this.smqQualifiers = input.qualifiers.filter(function (item) { return item.type.includes('SMQ'); });
+      relaQualifier = input.qualifiers.find(function (item) { return item.type == 'RELA'; });
+      this.qualifiers = input.qualifiers.filter(function (item) { return item.type != 'RELA'; });
+      console.log('xxx', this.qualifiers);
     }
 
 
@@ -29,23 +30,23 @@ export class Relationship {
 
     if (input.type == 'RB') {
       // If we're showing "Narrower Concepts" table
-      if (configService.getTerminology().terminology == 'ncim') {
-        this.rela = relaqualifier ? relaqualifier.value : "Narrower"
+      if (configService.isMultiSource && configService.isRrf()) {
+        this.type = relaQualifier ? relaQualifier.value : "Narrower"
       }
       // If we're showing "Associations" table
-      else {
-        this.rela = relaqualifier ? relaqualifier.value : "Broader"
+      else if (configService.isRrf()) {
+        this.type = relaQualifier ? relaQualifier.value : "Broader"
       }
     } else if (input.type == 'RN') {
-      if (configService.getTerminology().terminology == 'ncim') {
-        this.rela = relaqualifier ? relaqualifier.value : "Broader"
+      if (configService.isMultiSource && configService.isRrf()) {
+        this.type = relaQualifier ? relaQualifier.value : "Broader"
       }
       // If we're showing "Associations" table
-      else {
-        this.rela = relaqualifier ? relaqualifier.value : "Narrower"
+      else if (configService.isRrf()) {
+        this.type = relaQualifier ? relaQualifier.value : "Narrower"
       }
-    } else if (input.type.startsWith('R')) {
-      this.rela = relaqualifier ? relaqualifier.value : "Other"
+    } else if (input.type.startsWith('R') && configService.isRrf()) {
+      this.type = relaQualifier ? relaQualifier.value : "Other"
     }
   }
 }
