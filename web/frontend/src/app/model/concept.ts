09dd66353a91d50ae27997085630f8e48ae7bcc7
twkatadin <twkatadin@gmail.com>
2023-05-23 20:32:31 -1000

    EVSRESTAPI-227: Fixed ncim concepts to not be viewed as retired just because history is present

--- web/frontend/src/app/model/concept.ts
+++ web/frontend/src/app/model/concept.ts
@@ -104,7 +104,16 @@ export class Concept {
       }
       this.historyCt = this.getCt(this.history);
 
-      if (input.history.length > 0 && (input.terminology == "ncim" || (input.terminology == "ncit" && this.synonymsCt == 0))) {
+      if (input.history.length > 0 && input.terminology == "ncim") {
+
+        for (let history of input.history) {
+
+            if (history.code == input.code) {
+              this.retired = true;
+            }
+        }
+
+      } else if (input.history.length > 0 && input.terminology == "ncit" && this.synonymsCt == 0) {
         this.retired = true;
       }
     }
