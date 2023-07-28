53e9200641db3f18ec9a919277c9172ad5c2e7c4
peter-va <pvanaus@ucdavis.edu>
2023-07-27 17:59:17 -0700

    requested tweaks

--- web/frontend/src/app/model/searchResultTableFormat.ts
+++ web/frontend/src/app/model/searchResultTableFormat.ts
@@ -71,7 +71,8 @@ export class SearchResultTableFormat {
 
         const data = new TableData();
         data.column1 = searchResult.concepts[i].code;
-        data.terminology = searchResult.concepts[i]?.terminology;
+        const term = searchResult.concepts[i]?.terminology;
+        data.terminology = term ? configService.getTerminologyByName(term).metadata.uiLabel.replace(/\:.*/, "") : "";
         data.retiredConcept = !searchResult.concepts[i].active ? 'yes' : 'no';
         data.highlight = searchResult.concepts[i].getHighlightText();
         data.expanded = false;
