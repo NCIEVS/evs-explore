a912d70b54e1dd2dd8c16344a306efb5a92dff6c
Brian Carlsen <bcarlsen@westcoastinformatics.com>
2023-09-22 13:24:30 -0700

    EVSRESTAPI-382: license stuff (#248)
    * Fixes for ui-button-info->p-button-info, updates for Zuul to send ui license
    
    * Cleanup of buttons style
    
    * CLEANUP: fix small issue with sizing on search page
    
    * Fix new button

--- README.md
+++ README.md
@@ -74,3 +74,17 @@ The application has been specifically tested on the following browsers:The appli
 * Mozilla Firefox
 * Microsoft Edge
 * Opera
+
+### Setting up Prettier
+
+- Download the official Prettier extension (version 10.1.0 or above).
+- Make sure your ".prettierrc" file is in the root folder.
+- Go to settings in the vscode menu.
+- In the search bar, type "Prettier".
+- Scroll down to "Prettier: Config Path"
+- Type ".prettierrc" in the config path field.
+- Now navigate to any typescript file.
+- right click the code, and select "Format Document With...".
+- If Prettier is not the default, select "Configure Default Formatter..." and select Prettier.
+- Save your file, and and confirm that is prettier is working by checking the "prettier status" in the bottom right corner of the vsCode window.
+- If it is red, observe the error and troubleshoot the problem by clicking the red icon.
--- web/src/main/java/gov/nih/nci/evsexplore/web/filters/UiHeaderPreFilter.java
+++ web/src/main/java/gov/nih/nci/evsexplore/web/filters/UiHeaderPreFilter.java
@@ -1,13 +1,21 @@
 package gov.nih.nci.evsexplore.web.filters;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
 import org.springframework.stereotype.Component;
 
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 
+import gov.nih.nci.evsexplore.web.properties.WebProperties;
+
 @Component
 public class UiHeaderPreFilter extends ZuulFilter {
+
+  /** The web properties. */
+  @Autowired
+  WebProperties properties;
+
   @Override
   public int filterOrder() {
     return FilterConstants.PRE_DECORATION_FILTER_ORDER - 1;
@@ -26,7 +34,7 @@ public class UiHeaderPreFilter extends ZuulFilter {
   @Override
   public Object run() {
     final RequestContext ctx = RequestContext.getCurrentContext();
-    ctx.addZuulRequestHeader("X-EVSRESTAPI-License-Key", "ui-license");
+    ctx.addZuulRequestHeader("X-EVSRESTAPI-License-Key", properties.getUiLicense());
 
     return null;
   }
