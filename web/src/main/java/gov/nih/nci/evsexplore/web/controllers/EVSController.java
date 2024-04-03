2443eec905ef5955d5a2027c85738a8eec2e9c48
Alicia G. <agarcia@westcoastinformatics.com>
2024-04-03 09:49:57 -0700

    Added comments to new classes. Added tasks and jar back to gradle for jenkins build

--- web/src/main/java/gov/nih/nci/evsexplore/web/controllers/EVSController.java
+++ web/src/main/java/gov/nih/nci/evsexplore/web/controllers/EVSController.java
@@ -10,12 +10,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.logging.Logger;
+
+/**
+ * The controller for sending proxy request to the EVS REST API.
+
+ */
 @RestController
 public class EVSController {
+    /**
+     * The logger.
+     */
     Logger logger = Logger.getLogger(EVSController.class.getName());
 
+    /**
+     * The proxy service.
+     */
     @Autowired
     ProxyService service;
+
+    /**
+     * Send our request to the EVS REST API.
+     * @param body  The body of the request.
+     * @param method    The method of the request.
+     * @param request   The request.
+     * @param response  The response.
+     * @return  The response entity.
+     */
     @RequestMapping("api/v1/**")
     public ResponseEntity<String> sendRequestToEVSRestApi(
             @RequestBody(required = false) String body,
@@ -24,6 +44,7 @@ public class EVSController {
             HttpServletResponse response
     ) {
         logger.info("Received request to send to EVS REST API");
+        // Send the request to the proxy service
         return service.processProxyRequest(body, method, request, response);
     }
 }
--- web/src/main/java/gov/nih/nci/evsexplore/web/controllers/ProxyService.java
+++ web/src/main/java/gov/nih/nci/evsexplore/web/controllers/ProxyService.java
@@ -22,40 +22,63 @@ import java.net.URISyntaxException;
 import java.util.Enumeration;
 import java.util.logging.Level;
 
+/**
+ * Proxy Service for handling the requests to the EVS REST API.
+ */
 @Service
 public class ProxyService {
+    /**
+     * The logger.
+     */
     private static final Logger logger = LoggerFactory.getLogger(ProxyService.class);
 
+    /**
+     * The domain uri, pulled from our application.yml.
+     */
     @Value("${spring.cloud.gateway.routes[0].uri}")
     private String domain;
 
+    /**
+     * Proocess the proxy request and handle the various components we
+     * need to send with the request to the EVS REST API.
+     * @param body The body of the request.
+     * @param method The method of the request.
+     * @param request The request.
+     * @param response  The response.
+     * @return  The response entity.
+     */
     public ResponseEntity<String> processProxyRequest(
             String body,
             HttpMethod method,
             HttpServletRequest request,
             HttpServletResponse response
     ) {
+        // Get the request URL
         String requestUrl = request.getServletPath();
         logger.info("REQUEST PATH: " + requestUrl);
 
-        // replacing context path form urI to match actual gateway URI
+        // replacing context path from urI to match actual gateway URI
         URI uri = UriComponentsBuilder.fromUriString(domain)
                 .path(requestUrl)
                 .query(request.getQueryString())
                 .build(true).toUri();
 
+        // Create the headers for the request
         HttpHeaders headers = new HttpHeaders();
         Enumeration<String> headerNames = request.getHeaderNames();
 
+        //  Add the headers to the request
         while (headerNames.hasMoreElements()) {
             String headerName = headerNames.nextElement();
             headers.set(headerName, request.getHeader(headerName));
         }
 
+        // Create the request entity
         HttpEntity<String> httpEntity = new HttpEntity<>(body, headers);
         ClientHttpRequestFactory factory = new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory());
         RestTemplate restTemplate = new RestTemplate(factory);
 
+        // Send the request to the EVS REST API
         try {
             ResponseEntity<String> serverResponse = restTemplate.exchange(uri, method, httpEntity, String.class);
             HttpHeaders responseHeaders = new HttpHeaders();
