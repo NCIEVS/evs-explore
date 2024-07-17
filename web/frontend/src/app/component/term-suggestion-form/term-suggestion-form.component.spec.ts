38e029b791fafd9ee0bcacc3eaefb7e84c84f270
gaalici1990 <agarcia@westcoastinformatics.com>
2024-07-17 16:17:33 -0700

    EVSRESTAPI-462 replaced hardcoded recaptcha site key with form prop. Added tests

--- web/frontend/src/app/component/term-suggestion-form/term-suggestion-form.component.spec.ts
+++ web/frontend/src/app/component/term-suggestion-form/term-suggestion-form.component.spec.ts
@@ -1,6 +1,7 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {TermSuggestionFormComponent} from './term-suggestion-form.component';
+import {AbstractControl} from "@angular/forms";
 
 describe('TermSuggestionFormComponent', () => {
   let component: TermSuggestionFormComponent;
@@ -17,7 +18,38 @@ describe('TermSuggestionFormComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  // Test an instance is created
+  it('should create the component instance', () => {
     expect(component).toBeTruthy();
   });
+
+  // Test the form is initialized onInit
+  it('should initialize the form', () => {
+    // call the ngOnInit method to initialize the form & check if the formGroup is defined
+    component.ngOnInit();
+    expect(component.getFormGroup()).toBeDefined();
+  });
+
+  // Test the submit form logic is called
+  it('should submit the form', () => {
+    // spy on the onSubmit method
+    spyOn(component, 'onSubmit');
+    // call the onSubmit method & check if the onSubmit method has been called
+    component.onSubmit();
+    expect(component.onSubmit).toHaveBeenCalled();
+  });
+
+  it('should validate the form controls', () => {
+    // replace 'controlName' with the actual control name
+    const control: AbstractControl<any, any> = component.getFormGroup().get('controlName');
+    // set an invalid value
+    control.setValue('');
+    // check if the control is invalid
+    expect(control.valid).toBeFalsy();
+    // set a valid value
+    control.setValue('valid value');
+    // check if the control is valid
+    expect(control.valid).toBeTruthy();
+  });
+
 });
