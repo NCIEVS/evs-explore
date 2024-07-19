df0653936cedfb75cc200edadd339dccfa856aa2
Brian Carlsen <bcarlsen@westcoastinformatics.com>
2024-07-19 21:31:43 +0100

    RDFBROWSER-486: fix logic to allow nci maps to download properly

--- web/frontend/src/app/component/mappings/mappings.component.ts
+++ web/frontend/src/app/component/mappings/mappings.component.ts
@@ -1,18 +1,17 @@
-import {Component, OnInit} from '@angular/core';
-import {ConfigurationService} from 'src/app/service/configuration.service';
-import {saveAs} from 'file-saver';
-import {LoaderService} from 'src/app/service/loader.service';
-import {MapsetService} from 'src/app/service/mapset.service';
+import { Component, OnInit } from '@angular/core';
+import { ConfigurationService } from 'src/app/service/configuration.service';
+import { saveAs } from 'file-saver';
+import { LoaderService } from 'src/app/service/loader.service';
+import { MapsetService } from 'src/app/service/mapset.service';
 
 // Component for mappings.  Currently, this just redirects to another page.
 @Component({
   selector: 'app-mappings',
   templateUrl: './mappings.component.html',
-  styleUrls: ['./mappings.component.css']
+  styleUrls: ['./mappings.component.css'],
 })
 export class MappingsComponent implements OnInit {
-
-  constructor(private configService: ConfigurationService, private loaderService: LoaderService, private mapsetService: MapsetService) { }
+  constructor(private configService: ConfigurationService, private loaderService: LoaderService, private mapsetService: MapsetService) {}
 
   dummyList = ['one', 'two', 'three'];
   mappings: any = null;
@@ -24,18 +23,17 @@ export class MappingsComponent implements OnInit {
   MAX_PAGE = 10000;
 
   ngOnInit() {
-    this.mapsetService.getMapsets('properties').subscribe(response => {
+    this.mapsetService.getMapsets('properties').subscribe((response) => {
       this.mappings = response;
-      this.mappings.forEach(map => {
+      this.mappings.forEach((map) => {
         this.nameToCodeMaps.push(map.name);
         this.nameToCodeMaps[map.name] = map.code;
-        if (map.properties.find(obj => obj.type === 'downloadOnly' && obj.value === 'false')) {
+        if (map.properties.find((obj) => obj.type === 'downloadOnly' && obj.value === 'false')) {
           this.viewMappings.push(map.name);
           if (map.version) {
             this.versionMapping[map.name] = map.version;
           }
-        }
-        else {
+        } else {
           this.downloadMappings.push(map.name);
           if (map.version) {
             this.versionMapping[map.name] = map.version;
@@ -54,14 +52,13 @@ export class MappingsComponent implements OnInit {
   }
 
   async downloadMapping(mapName: string) {
-    this.mapsetService.getMapsetByCode(mapName, 'properties').subscribe(response => {
+    this.mapsetService.getMapsetByCode(mapName, 'properties').subscribe((response) => {
       const mapset = response;
       const properties = mapset['properties'];
-      if (properties.find(obj => obj.type === 'mapsetLink' && obj.value === null)) {
+      if (properties.find((obj) => obj.type === 'mapsetLink' && !obj.value)) {
         this.downloadStoredMapping(mapName);
-      }
-      else if (properties.find(obj => obj.type === 'mapsetLink' && obj.value != null)) {
-        window.open(properties.find(obj => obj.type === 'mapsetLink').value);
+      } else if (properties.find((obj) => obj.type === 'mapsetLink' && obj.value)) {
+        window.open(properties.find((obj) => obj.type === 'mapsetLink').value);
       }
     });
   }
@@ -70,37 +67,44 @@ export class MappingsComponent implements OnInit {
     this.loaderService.showLoader();
     let mappingText = '';
     let total = 0;
-    await this.mapsetService.getMapsetMappings(mapName)
-      .toPromise().then(response => {
+    await this.mapsetService
+      .getMapsetMappings(mapName)
+      .toPromise()
+      .then((response) => {
         total = response['total'];
       });
     const pages = Math.ceil(total / this.MAX_PAGE);
     for (let i = 0; i < pages; i++) {
-      await this.mapsetService.getMapsetMappings(mapName, Math.min(this.MAX_PAGE, total - i * this.MAX_PAGE), i * this.MAX_PAGE)
-        .toPromise().then(response => {
+      await this.mapsetService
+        .getMapsetMappings(mapName, Math.min(this.MAX_PAGE, total - i * this.MAX_PAGE), i * this.MAX_PAGE)
+        .toPromise()
+        .then((response) => {
           const mapsetMappings = response['maps'];
-          mapsetMappings.forEach(map => {
+          mapsetMappings.forEach((map) => {
             // get titles and pretty-fy them
             if (mappingText === '') {
               // first replace: split words by lowercase letter -> uppercase letter
               // second replace: capitalize new first word
-              mappingText += Object.keys(map).map((fieldName) =>
-                fieldName.replace(/([a-z])([A-Z])/g, '$1 $2')
-                  .replace(/^\w/, c => c.toUpperCase())).join(',') + '\r\n';
+              mappingText +=
+                Object.keys(map)
+                  .map((fieldName) => fieldName.replace(/([a-z])([A-Z])/g, '$1 $2').replace(/^\w/, (c) => c.toUpperCase()))
+                  .join(',') + '\r\n';
             }
             mappingText += this.exportCodeFormatter(map) + '\r\n';
           });
         });
     }
-    saveAs(new Blob([mappingText], {
-      type: 'text/plain'
-    }), mapName + new Date().toISOString() + '.csv');
+    saveAs(
+      new Blob([mappingText], {
+        type: 'text/plain',
+      }),
+      mapName + new Date().toISOString() + '.csv',
+    );
     this.loaderService.hideLoader();
   }
 
   exportCodeFormatter(map: object) {
     // extraneous commas are the bane of my existence
-    return '\"' + Object.values(map).join('\",\"') + '\"';
+    return '"' + Object.values(map).join('","') + '"';
   }
-
 }
