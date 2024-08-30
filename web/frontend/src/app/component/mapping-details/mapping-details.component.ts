172ea12284c96883b01a14ed7bc7cbcd43717a47
Peter Van Ausdeln <pvanausdeln@westcoastinformatics.com>
2024-08-30 10:01:31 -0700

    Pva/rdfbrowser 490 show map rule (#299)
    * add toggle for rules in mapping page
    
    * only show rule button for snomed

--- web/frontend/src/app/component/mapping-details/mapping-details.component.ts
+++ web/frontend/src/app/component/mapping-details/mapping-details.component.ts
@@ -30,6 +30,7 @@ export class MappingDetailsComponent implements OnInit {
   MAX_PAGE = 10000;
   termAutoSearch: string;
   textSuggestions: any;
+  showRules: boolean = false;
 
   conceptUrlBase = '/concept';
   sourceTermSaved: boolean = false;
@@ -131,6 +132,19 @@ export class MappingDetailsComponent implements OnInit {
     if (this.mappings) {
       this.mappings._first = 0;
     }
+  }
+
+  // toggle rule showing
+  showOrHideRules(): any {
+    this.showRules = !this.showRules;
+  }
+
+  get getRulesToggleString(): string {
+    return this.showRules ? 'Hide Rules' : 'Show Rules';
+  }
+
+  rulesExist() {
+    return this.mapsetCode.startsWith('SNOMED');
   }
 
   // Handle lazy loading of table
