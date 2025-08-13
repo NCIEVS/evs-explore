27417e37182f449672b7736e5b0e561cd65f8af6
Brian Carlsen <bcarlsen@westcoastinformatics.com>
2025-08-13 09:31:19 -0700

    Fix github trivy vulnerability with org.springframework.cloud:spring-cloud-gateway-server-mvc

--- web/src/main/java/gov/nih/nci/evsexplore/web/configuration/StaticResourcesConfiguration.java
+++ web/src/main/java/gov/nih/nci/evsexplore/web/configuration/StaticResourcesConfiguration.java
@@ -1,5 +1,8 @@
-
 package gov.nih.nci.evsexplore.web.configuration;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.boot.autoconfigure.web.WebProperties;
 import org.springframework.boot.autoconfigure.web.WebProperties.Resources;
@@ -11,77 +14,71 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.concurrent.TimeUnit;
-
-/**
- * Static resource config.
- */
+/** Static resource config. */
 @Configuration
-@EnableConfigurationProperties({
-        WebProperties.class
-})
+@EnableConfigurationProperties({WebProperties.class})
 public class StaticResourcesConfiguration implements WebMvcConfigurer {
 
-    /**
-     * The Constant STATIC_RESOURCES. Need to be explicitly listed to ensure our routes will
-     * set the MIME type correctly
-     */
+  /**
+   * The Constant STATIC_RESOURCES. Need to be explicitly listed to ensure our routes will set the
+   * MIME type correctly
+   */
+  static final String[] STATIC_RESOURCES =
+      new String[] {
+        "/**/*.css", "/**/*.html", "/**/*.js", "/**/*.json", "/**/*.bmp",
+        "/**/*.jpeg", "/**/*.jpg", "/**/*.png", "/**/*.ttf", "/**/*.eot",
+        "/**/*.svg", "/**/*.woff", "/**/*.woff2", "/**/*.xls"
+      };
 
-    static final String[] STATIC_RESOURCES = new String[] {
-            "/**/*.css", "/**/*.html", "/**/*.js", "/**/*.json", "/**/*.bmp",
-            "/**/*.jpeg", "/**/*.jpg", "/**/*.png", "/**/*.ttf", "/**/*.eot",
-            "/**/*.svg", "/**/*.woff", "/**/*.woff2", "/**/*.xls"
-    };
+  /** The resource properties. */
+  private final Resources resourceProperties = new Resources();
 
-    /**
-     * The resource properties.
-     */
-    private final Resources resourceProperties = new Resources();
+  /**
+   * Add handlers for serving static resources from /static/ folder and implement Single Page
+   * Application (SPA) routing.
+   *
+   * @param registry the resource handler registry
+   */
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    // Set the cache period
+    Long cachePeriodLong = 30L;
+    int cachePeriodInt = cachePeriodLong.intValue();
+    CacheControl cacheControl = CacheControl.maxAge(cachePeriodLong, TimeUnit.SECONDS);
+    Integer cachePeriod = cachePeriodInt;
 
-    /**
-     * Add handlers for serving static resources from /static/ folder and
-     * implement Single Page Application (SPA) routing.
-     *
-     * @param registry the resource handler registry
-     */
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // Set the cache period
-        Long cachePeriodLong = 30L;
-        int cachePeriodInt = cachePeriodLong.intValue();
-        CacheControl cacheControl = CacheControl.maxAge(cachePeriodLong, TimeUnit.SECONDS);
-        Integer cachePeriod = cachePeriodInt;
+    // Load all static resources
+    registry
+        .addResourceHandler(STATIC_RESOURCES)
+        .addResourceLocations(resourceProperties.getStaticLocations())
+        .setCacheControl(cacheControl)
+        .resourceChain(true);
 
-        // Load all static resources
-        registry.addResourceHandler(STATIC_RESOURCES)
-                .addResourceLocations(resourceProperties.getStaticLocations())
-                .setCacheControl(cacheControl)
-                .resourceChain(true);
+    // Create mapping to index.html for Angular HTML5 mode. All urls route to the index page (SPA).
+    String[] indexLocations = getIndexLocations();
+    registry
+        .addResourceHandler("/**")
+        .addResourceLocations(indexLocations)
+        .setCachePeriod(cachePeriod)
+        .resourceChain(true)
+        .addResolver(
+            new PathResourceResolver() {
+              @Override
+              protected Resource getResource(String resourcePath, Resource location)
+                  throws IOException {
+                return location.exists() && location.isReadable() ? location : null;
+              }
+            });
+  }
 
-        // Create mapping to index.html for Angular HTML5 mode. All urls route to the index page (SPA).
-        String[] indexLocations = getIndexLocations();
-        registry.addResourceHandler("/**")
-                .addResourceLocations(indexLocations)
-                .setCachePeriod(cachePeriod)
-                .resourceChain(true)
-                .addResolver(new PathResourceResolver() {
-                    @Override
-                    protected Resource getResource(String resourcePath, Resource location)
-                            throws IOException {
-                        return location.exists() && location.isReadable() ? location : null;
-                    }
-                });
-    }
-
-    /**
-     * Returns the index locations.
-     *
-     * @return the index locations
-     */
-    private String[] getIndexLocations() {
-        return Arrays.stream(resourceProperties.getStaticLocations())
-                .map((location) -> location + "index.html").toArray(String[]::new);
-    }
+  /**
+   * Returns the index locations.
+   *
+   * @return the index locations
+   */
+  private String[] getIndexLocations() {
+    return Arrays.stream(resourceProperties.getStaticLocations())
+        .map((location) -> location + "index.html")
+        .toArray(String[]::new);
+  }
 }
