fd3a6190811600915a2f4ad7aac50d8dbc620877
Amanda Sinha <36523911+amandasinha@users.noreply.github.com>
2025-12-04 16:51:48 -0800

    Ams/rdfbrowser 446 term form file attachment (#388)
    * temporarily added custom form const for cdisc in term-form-suggestion component.
    
    * Currently still has testing stuff for CDISC form, contains visual bug with regards to required status
    
    * Fixed visual bug for requirements, no longer mocking data. Functionality works.
    
    * Fixed slight bug that NCIT form wasn't working. Removed testing stuff.
    
    * quick edit to error message
    
    * Fix trivy scan failing and jest test.
    
    * Error message on frontend.
    
    * For tag making
    
    ---------
    
    Co-authored-by: peter-va <pvanaus@ucdavis.edu>

--- frontend/src/app/model/evsError.ts
+++ frontend/src/app/model/evsError.ts
@@ -6,11 +6,28 @@ export class EvsError extends Error {
     // Error message
     displayMessage: string;
 
-    constructor(error: Error, displayMessage: string) {
+    constructor(error: any, displayMessage: string) {
         super(error.message);
         // Set the prototype explicitly.
         Object.setPrototypeOf(this, EvsError.prototype);
         this.displayMessage = displayMessage;
+
+        // strictly for term form
+        if (displayMessage === 'An error occurred submitting the form'){
+            // account for if we're getting back an error message from backend
+            if (error && typeof error.error === 'object') {
+                if (error.error.message) {
+                    this.displayMessage = error.error.message;
+                }
+                else if (error.error.error) {
+                    this.displayMessage = error.error.error;
+                }
+            }
+            // or if we're getting a simpler error
+            else if (error && typeof error.error === 'string') {
+                this.displayMessage = error.error
+            }
+        }
 
         // Display the notification by default
         this.displayNotification = true;
