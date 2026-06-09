7431e45485db9054095362b5c05691dbf67d764a
amandasinha <amanda.m.sinha@gmail.com>
2026-06-09 15:49:21 -0700

    Removed search call from lazy load since search already happens on ngAfterViewInit

--- frontend/src/app/component/general-search/general-search.component.ts
+++ frontend/src/app/component/general-search/general-search.component.ts
@@ -358,7 +358,6 @@ export class GeneralSearchComponent implements OnInit, OnDestroy, AfterViewInit 
       this.pageSize = event.rows;
       this.setQueryUrl();
       this.avoidLazyLoading = true;
-      this.performSearch();
     }
   }
 
