50a7188057f8717c0dc64b02b1d82a8ef34e77a3
amandasinha <amanda.m.sinha@gmail.com>
2026-08-17 08:33:19 -0700

    Changed role relationship initialization to use mapping instead of looping over. De-duplicates any roles if they match on all props except group.

--- frontend/src/app/model/concept.ts
+++ frontend/src/app/model/concept.ts
@@ -156,13 +156,8 @@ export class Concept {
     // roles
     this.roles = new Array();
     if (input.roles) {
-      for (let i = 0; i < input.roles.length; i++) {
-        
-        // Deal with duplicate roles that occur with groups, add just the one with the group
-        if (input.roles[i]?.relatedCode == input.roles[i+1]?.relatedCode && input.roles[i+1]?.group) {  i++;  }
-
-        this.roles.push(new Relationship(input.roles[i], configService));
-      }
+      const key = o => JSON.stringify([o.relatedCode, o.relatedName, o.source, o.type]); 
+      this.roles = [...new globalThis.Map<string, Relationship>(input.roles.map(o => [key(o), o])).values()]; 
       this.rolesCt = this.getCt(this.roles);
     }
 
